ca9bf6547a5c2d8d052ee8e64695e866feea041b
b08505018 <sml890115@gmail.com>
2022-03-08 13:19:37 +0800

    Update 70_Dp.cpp

--- 70_Climbing Stairs/70_Dp.cpp
+++ 70_Climbing Stairs/70_Dp.cpp
@@ -3,7 +3,18 @@ public:
     int climbStairs(int n) {
         // DP
         // 想法1: 每多加入一個長度則有規則的增加方法數?
+        // => 規律 n = n-1 + n-2?
+        if(n == 2) return 2;
+        if(n == 1) return 1;
         
-        
+        int n_2 = 1;
+        int n_1 = 2;
+        int f;
+        for(int i = 2; i < n;i++){
+            f = n_2 + n_1;
+            n_2 = n_1;
+            n_1 = f;
+        }
+        return f;
     }
 };
