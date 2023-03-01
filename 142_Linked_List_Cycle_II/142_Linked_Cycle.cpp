b620bb7d1b83e75f685f8276cd0d02ce822f8057
b08505018 <sml890115@gmail.com>
2023-03-01 12:41:34 +0800

    Create 142_Linked_Cycle.cpp

---
+++ 142_Linked_List_Cycle_II/142_Linked_Cycle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+/*
+note: 
+    
+improve:
+    
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+    }
+};
+
+// test function
+int main(){
+    // given values
+    vector<int> nums = {0};
+    int target = 0;
+
+    // call solution function
+    Solution s;
+    int ans = s.function("arguments");
+
+    // print out answers 
+    cout << ans;
+
+    return 0;
+}
