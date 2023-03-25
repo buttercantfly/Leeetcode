356ed090f65549dffc00968dac16506c188a1fd7
buttercantfly <sml890115@gmail.com>
2023-03-25 16:48:05 +0800

    347 finished

--- 347_Top_K_Frequent_Elements/347_Top_K_Frequent_Elements.cpp
+++ 347_Top_K_Frequent_Elements/347_Top_K_Frequent_Elements.cpp
@@ -4,6 +4,7 @@
 #include <deque>
 #include <algorithm>
 #include <unordered_map>
+#include <queue>
 using namespace std;
 
 /*
@@ -47,26 +48,50 @@ note:
     };
 
 improve:
-    
+
+    多熟悉c++內建的stl以及iterator用法
+
 */
 
 // Class Solution copy here
 class Solution {
 public:
+    // 自定義 comparison operator()
+    // 注意基本上名稱是用左右node的想法 (lhs vs rhs)
+    // 本題應該是要比較second(出現頻率)，並且低的較高順位 => 左大回傳true 左小回傳false
+    class mycomparison {
+    public:
+        bool operator()(const pair<int, int>& lhs, const pair<int, int>& rhs) {
+            return lhs.second > rhs.second;
+        }
+    };
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> map;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, mycomparison> pque;
+        vector<int> ret;
 
+        // hash map 紀錄各數字出現次數
         for (int i: nums)
         {
             if (map.count(i)) map[i] += 1;
             else map[i] = 1;
         }
         
+        // 統計完後將所有元素push進pque，並確保其維持<k的大小
         for (pair<int, int> p: map)
         {
-            int c = p.second;
+            pque.push(p); 
+            if (pque.size() > k) pque.pop();
         }
         
+        while (!pque.empty())
+        {
+            ret.push_back(pque.top().first);
+            pque.pop();
+        }
+        
+        return ret;
     }
 };
 
