1d79fe1be770c422509a6594b05a652e6b59bc64
b08505018 <sml890115@gmail.com>
2023-09-07 16:49:32 +0800

    701 done
    開始結束補中間

---
+++ 701_BST_Insertion/701_BST_Insertion_iterative.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+/*
+question:
+note:
+    
+improve:
+    
+*/
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Class Solution copy here
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) return new TreeNode(val);
+        
+        TreeNode* cur = root;
+        TreeNode* parent;
+        while (cur)
+        {
+            parent = cur;
+            if (cur->val > val)
+            {
+                cur = cur->left;
+                if (!cur) parent->left = new TreeNode(val);
+            }
+            else
+            {
+                cur = cur->right;
+                if (!cur) parent->right = new TreeNode(val);
+            }
+        }
+        return root;
+    }
+};
+
+// test function
+class Builder
+{
+private:
+    /* data */
+public:
+    TreeNode* build_tree(vector<int>& vec, int position)
+    {
+        if(position > vec.size()) return nullptr;
+        TreeNode* root = new TreeNode(vec[position - 1]);
+        root->left  = build_tree(vec, position*2);     
+        root->right = build_tree(vec, position*2+1);
+
+        return root;
+    }
+};
+
+// preorder print
+void preorder_print(TreeNode* root){
+    cout << root->val;
+    if(root->left) preorder_print(root->left);
+    if(root->right) preorder_print(root->right);
+}
+
+int main(){
+
+    vector<int> vec = {1,2,3,4,5,6,7};
+    Builder builder;
+    TreeNode* root = builder.build_tree(vec, 1);
+    
+    preorder_print(root); 
+
+    return 0;
+}
---
+++ 701_BST_Insertion/701_BST_Insertion_recursive.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+/*
+question:
+note:
+    遞迴三部曲
+    1. 確定遞迴參數與返回值(開始)
+    2. 確定終止條件(結束)
+    3. 確定單層邏輯(中間)
+    =>開始結束補中間
+    
+improve:
+    
+*/
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Class Solution copy here
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if (!root) return new TreeNode(val);
+        if (root->val > val) root->left  = insertIntoBST(root->left,  val);
+        if (root->val < val) root->right = insertIntoBST(root->right, val);
+        return root;
+    }
+};
+
+// test function
+class Builder
+{
+private:
+    /* data */
+public:
+    TreeNode* build_tree(vector<int>& vec, int position)
+    {
+        if(position > vec.size()) return nullptr;
+        TreeNode* root = new TreeNode(vec[position - 1]);
+        root->left  = build_tree(vec, position*2);     
+        root->right = build_tree(vec, position*2+1);
+
+        return root;
+    }
+};
+
+// preorder print
+void preorder_print(TreeNode* root){
+    cout << root->val;
+    if(root->left) preorder_print(root->left);
+    if(root->right) preorder_print(root->right);
+}
+
+int main(){
+
+    vector<int> vec = {1,2,3,4,5,6,7};
+    Builder builder;
+    TreeNode* root = builder.build_tree(vec, 1);
+    
+    preorder_print(root); 
+
+    return 0;
+}
