89f456cc2c5d735ec8a63423d42a85779de9d7b0
buttercantfly <sml890115@gmail.com>
2024-03-31 22:19:24 +0800

    272 suppose to AC?
    一直跳CE(compile error)，連解答都一樣?

--- CPE_272/CPE_272.cpp
+++ CPE_272/CPE_272.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
 using namespace std;
 
-/*
-note:
-    把左雙引號(")變成
-    =>``左雙引號
-    以及右雙引號(")變成
-    =>''右雙引號
-*/
- 
-int main() {
-    bool isLeft;
-    string str;
-    string strA;
+bool isLeft;
+string str;
 
-    while (cin >> str)
+int main() {
+    isLeft = true;
+
+    while (getline(cin, str))
     {
-        isLeft = true;
-        strA = "";
-        
         for (char c:str)
         {
-            if (c == '"' && isLeft)
+            if (c == '\"' && isLeft)
             {
-                strA += "``";
+                cout << "``";
                 isLeft = false;
             }
-            else if (c == '"' && !isLeft)
+            else if (c == '\"' && !isLeft)
             {
-                strA += "''";
+                cout << "''";
                 isLeft = true;
             }
-            else
-            {
-                strA += c;
-            }
+            else cout << c;
         }
-        cout << strA << endl;
+        cout << "\n";
     }
 
     return 0;
