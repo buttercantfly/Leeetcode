095d9fdfb0de42c5794b3d0b6ae66a5d353f1340
b08505018 <sml890115@gmail.com>
2025-02-26 14:01:56 +0800

    2096

---
+++ 2096_Daily0716/2096.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+/*
+question:
+note:
+    用遞迴跑
+    1. 參數
+    2. 終止
+    3. 過程
+improve:
+    
+*/
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// Class Solution copy here
+class Solution {
+public:
+    string getDirections(TreeNode* root, int startValue, int destValue) {
+         
+    }
+
+    bool getPath(TreeNode* cur, int destVal, string& path) {
+        if (cur->val == destVal) return true;
+        if(!cur->left && !cur->right) return false;
+
+        if (cur->left && getPath(cur->left, destVal, path)) {
+            path += "L";
+            return true;
+        }
+
+        if (cur->right && getPath(cur->right, destVal, path)) {
+            path += "R";
+            return true;
+        }
+        
+        return false;
+    }
+};
+
+// test function
+class Builder
+{
+private:
+    /* data */
+public:
+    TreeNode* build_tree(vector<int>& vec, int position)
+    {
+        if(position > vec.size()) return nullptr;
+        TreeNode* root = new TreeNode(vec[position - 1]);
+        root->left  = build_tree(vec, position*2);     
+        root->right = build_tree(vec, position*2+1);
+
+        return root;
+    }
+};
+
+// preorder print
+void preorder_print(TreeNode* root){
+    cout << root->val;
+    if(root->left) preorder_print(root->left);
+    if(root->right) preorder_print(root->right);
+}
+
+int main(){
+
+    vector<int> vec = {1,2,3,4,5,6,7};
+    Builder builder;
+    TreeNode* root = builder.build_tree(vec, 1);
+    
+    preorder_print(root); 
+
+    return 0;
+}
